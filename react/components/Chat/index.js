adf0e74a3ca2192a26ece52f418000c7e5f0ff6c
LizethRivera04 <liza_teza94@hotmail.com>
2021-04-19 13:35:56 -0500

    Chat component added

--- react/components/Chat/index.js
+++ react/components/Chat/index.js
@@ -7,32 +7,12 @@ const Chat = () => {
 
   const handleClick = (e) => {
     MyG2C.API.chatAsk("Solicitud de ayuda.", 10);
-    /* setBubble(!bubble);
-    let image = e.target.src
-    if (image.includes('bubb')) {
-      MyG2C.API.chatAsk("Solicitud de ayuda.", 10);
-      setbox(true)
-    } else if (image.includes('open')) {
-      console.log('cerrar');
-      setbox(false)
-    } */
   }
 
   return (
-
     <div className="float">
       <span className="img-btn" onClick={handleClick}></span>
     </div>
   );
 };
-/* <>
-     {box && <div className="box">Chat</div>}
-     <div className="float">
-       <div>
-         {bubble ? <img src="https://siman.vtexassets.com/arquivos/chat-bubb.png" width="30%" onClick={handleClick} />
-           : <img src="https://siman.vtexassets.com/arquivos/chat-open.png" width="30%" onClick={handleClick} />
-         }
-       </div>
-     </div>
-   </> */
 export default Chat;
